d9cfc5bd26105d2d4bc2f87a7a29e1891e07c08a
Nosov Kirill <Kirillnosov7@gmail.com>
2026-07-22 01:54:45 +0300

    Initial commit

---
+++ Types_task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types.Task.One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialDeposit = double.Parse(Console.ReadLine());
+            int years = int.Parse(Console.ReadLine());
+            double interestRate = double.Parse(Console.ReadLine());
+            CalculateSavings(initialDeposit, years, interestRate);
+
+        }
+        static void CalculateSavings(double initialDeposit, int years, double interestRate)
+        {
+            double currentSavings = initialDeposit;
+            for (int i = 0; i < years; i++)
+            {
+                currentSavings = (currentSavings / interestRate) + currentSavings;
+                Console.WriteLine($"Год {i + 1}: {Math.Round(currentSavings, 2):F2} руб.");
+            }
+        }
+    }
+}
---
+++ Types_task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types.Task.Two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            DrawDiamond(N);
+        }
+        static void DrawDiamond(int N)
+        {
+
+            int center = N / 2;
+            for (int i = 0; i < N; i++)
+            {
+                StringBuilder emptyString = new StringBuilder(new string(' ', N));
+                int offset;
+                if (i <= center)
+                {
+                    offset = i;
+                    emptyString[center - offset] = 'X';
+                    emptyString[center + offset] = 'X';
+                }
+                if (i > center)
+                {
+                    offset = i - center;
+                    emptyString[offset] = 'X';
+                    emptyString[N - 1 - offset] = 'X';
+                }
+                Console.WriteLine(emptyString.ToString());
+            }
+        }
+    }
+}
