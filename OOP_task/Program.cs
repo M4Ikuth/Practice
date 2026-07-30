cb23260f416781c79efcca8571af5efa98d8a238
Nosov Kirill <Kirillnosov7@gmail.com>
2026-07-30 17:09:45 +0300

    OOP task

---
+++ OOP_task/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.task
+{
+    struct Point
+    {
+        double x, y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class Rectangle
+    {
+        public Point TopLeft { get; }
+        private double _width, _height;
+        public Rectangle(double x, double y, double width, double height)
+        {
+            TopLeft = new Point(x, y);
+
+            if (width < 0)
+            {
+                throw new ArgumentOutOfRangeException("Недопустимое значение ширины");
+            }
+            else
+                this._width = width;
+            if (height < 0)
+            {
+                throw new ArgumentOutOfRangeException("Недопустимое значение высоты");
+            }
+            else
+                this._height = height;
+        }
+        public double Area
+        {
+            get { return _height * _width; }
+        }
+        public double Perimeter
+        {
+            get { return (_height + _width) * 2; }
+        }
+
+        public static Rectangle ReadRectangle()
+        {
+            Console.WriteLine("Внесите параметры прямоугольника. Введите координаты левого верхнего угла прямоугольника (X,Y)");
+            string[] coordinates = Console.ReadLine().Split();
+            double x = double.Parse(coordinates[0]);
+            double y = double.Parse(coordinates[1]);
+            Console.WriteLine("Введите ширину и длинну");
+            string[] widthAndHeight = Console.ReadLine().Split();
+            double width = double.Parse(widthAndHeight[0]);
+            double height = double.Parse(widthAndHeight[1]);
+            return new Rectangle(x, y, width, height);
+        }
+
+        public static void DisplayInfoAboutRectangle(Rectangle rectangle)
+        {
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.Perimeter}");
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.Area}");
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Сколько объектов вы хотите создать?");
+                int amountOfRectangles = int.Parse(Console.ReadLine());
+                Rectangle[] rectangles = new Rectangle[amountOfRectangles];
+                for (int i = 0; i < amountOfRectangles; i++)
+                {
+                    rectangles[i] = Rectangle.ReadRectangle();
+                }
+                Console.WriteLine("Что вы желаете сделать? 1 - отобразить все объекты, 2 - показать прямоугольник с наибольшей площадью, 3 - показать прямоугольник с наибольшим периметром");
+                string answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        for (int i = 0; i < amountOfRectangles; i++)
+                        {
+                            Rectangle.DisplayInfoAboutRectangle(rectangles[i]);
+                        }
+                        break;
+                    case "2":
+                        double maxArea = rectangles[0].Area;
+                        int maxAreaRectangle = 0;
+                        for (int i = 0; i < amountOfRectangles; i++)
+                        {
+                            if (maxArea < rectangles[i].Area)
+                            {
+                                maxArea = rectangles[i].Area;
+                                maxAreaRectangle = i + 1;
+                            }
+                        }
+                        Console.WriteLine($"Прямоугольник с наибольшей площадью: {maxAreaRectangle}.\nЕго площадь: {maxArea}");
+                        break;
+                    case "3":
+                        double maxPerimeter = rectangles[0].Perimeter;
+                        int maxPerimeterRectangle = 0;
+                        for (int i = 0; i < amountOfRectangles; i++)
+                        {
+                            if (maxPerimeter < rectangles[i].Perimeter)
+                            {
+                                maxPerimeter = rectangles[i].Perimeter;
+                                maxPerimeterRectangle = i + 1;
+                            }
+                        }
+                        Console.WriteLine($"Прямоугольник с наибольшим периметром: {maxPerimeterRectangle}.\nЕго периметр: {maxPerimeter}");
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+    }
+}
---
+++ OOP_task/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("OOP_task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOP_task")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c6526310-1897-42c8-8523-afe38adc866e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
